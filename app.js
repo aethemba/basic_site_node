19a5a9d6980317fd64fa8d8e6d0989ba2ac37cb9
Aksel <aksel.ethem@gmail.com>
2013-01-06 17:01:05 +0100

    first commit

--- app.js
+++ app.js
@@ -28,7 +28,14 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+
+app.get('/', function(req, res) {
+    res.render('index', {
+      title: 'Home'
+    });
+});
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
